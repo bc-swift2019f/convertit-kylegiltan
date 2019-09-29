554f85a85ddaa8e1fd02d7584382b704aacdcad0
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-09-29 17:51:25 -0400

    6.4

--- convertIt/ViewController.swift
+++ convertIt/ViewController.swift
@@ -15,10 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
     
-    var formulaArray = ["miles to kilometers","kilometers to miles","feet to meters", "yars to meters", "meters to feet", "meters to yards"]
+    var formulaArray = ["miles to kilometers","kilometers to miles","feet to meters", "yards to meters", "meters to feet", "meters to yards"]
     
     var fromUnits = ""
     var toUnits = ""
+    var conversionString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,16 +28,37 @@ class ViewController: UIViewController {
         formulaPicker.dataSource = self
     }
 
-    
+    func calculateConversion(){
+        var outputValue = 0.0
+        if let inputValue = Double(userInput.text!){
+            switch conversionString{
+            case "miles to kilometers":
+                outputValue = inputValue / 0.62137
+            case "kilometers to miles":
+                outputValue = inputValue * 0.62137
+            case "feet to meters":
+                outputValue = inputValue / 3.2808
+            case "yards to meters":
+                outputValue = inputValue / 1.0936
+            case "meters to feet":
+                outputValue = inputValue * 3.2808
+            case "meters to yards":
+                outputValue = inputValue * 1.0936
+            default:
+                print("show alert")
+            }
+            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
+        }
+        else{
+            print("The value entered was not a number")
+        }
+    }
     
     @IBAction func convertButtonPressed(_ sender: UIButton) {
     }
     
-    
-    
-    
-
 }
+
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -52,12 +74,15 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        conversionString = formulaArray[row]
         let unitsArray = formulaArray[row].components(separatedBy: " to ")
         fromUnits = unitsArray[0]
         toUnits = unitsArray[1]
         fromUnitsLabel.text = formulaArray[row]
         fromUnitsLabel.text = fromUnits
         resultsLabel.text = toUnits
+        calculateConversion()
     }
     
 }
+
